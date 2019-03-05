7a63b981147b79b34cbb557c6d400dbb2239fa67
Yuchen Li <ychnlgy@gmail.com>
2019-03-05 16:41:43 -0500

    Uploaded files

--- src/main.py
+++ src/main.py
@@ -2,13 +2,15 @@ import dataset, model, train
 
 if __name__ == "__main__":
 
+    device = ["cpu", "cuda"][torch.cuda.is_available()]
+
     N = 512
     D = 32
 
     data = dataset.create(N, D)
     test = dataset.test(N, D)
 
-    net = model.EvolutionaryModel(D)
+    net = model.EvolutionaryModel(D).to(device)
 
     for i in range(100):
         net.do_cycle(*data, *test)
